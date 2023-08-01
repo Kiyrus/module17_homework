54d1bd3743c1c55a0ec10186f87064afe546df03
Ilya Kosov <kosov.ilya@bk.ru>
2023-08-01 19:41:39 +0300

    minor fixes in list with persons

--- source/__tests__/infoAboutPerson.test.js
+++ source/__tests__/infoAboutPerson.test.js
@@ -2,46 +2,294 @@ import { getAgeInfoFromList } from "../getAgeInfoFromList.js";
 
 describe("tests for infoAboutPerson", () => {
     let personsList = [
-        { name: "Walter", age: 45 },
-        { name: "Skyler", age: 40 },
-        { name: "Jesse", age: 37 },
-        { name: "Hank", age: 43 },
-        { name: "Marie", age: 39 },
-        { name: "Saul", age: 46 },
-        { name: "Mike", age: 34 },
-        { name: "Tuco", age: 55 },
-        { name: "Lydia", age: 30 },
-        { name: "Todd", age: 31 },
-        { name: "Kim", age: 33 },
-        { name: "Gustavo", age: 29 },
-        { name: "Howard", age: 36 },
+        {
+            name: "Alberto Véga",
+            age: 40,
+            gender: "male",
+        },
+        {
+            name: "Bob Odenkirk",
+            age: 50,
+            gender: "male",
+        },
+        {
+            name: "Govard Muldoon",
+            age: 60,
+            gender: "male",
+        },
+        {
+            name: "Jesse Pinkman",
+            age: 30,
+            gender: "male",
+        },
+        {
+            name: "Hank Moody",
+            age: 35,
+            gender: "male",
+        },
+        {
+            name: "Marisol Tomei",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Raymond Chandler",
+            age: 65,
+            gender: "male",
+        },
+        {
+            name: "Walt Warren",
+            age: 60,
+            gender: "male",
+        },
+        {
+            name: "José Guadalupe Posada Carrillo",
+            age: 70,
+            gender: "male",
+        },
+        {
+            name: "Jack Bauer",
+            age: 30,
+            gender: "male",
+        },
+        {
+            name: "Aaron Paul",
+            age: 40,
+            gender: "male",
+        },
+        {
+            name: "Mia Farrow",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Lucas Hedges",
+            age: 60,
+            gender: "male",
+        },
+        {
+            name: "Joanne Whalen",
+            age: 65,
+            gender: "female",
+        },
+        {
+            name: "Walter White",
+            age: 55,
+            gender: "male",
+        },
+        {
+            name: "Oscar-winning actor Walter White",
+            age: 55,
+            gender: "male",
+        },
+        {
+            name: "Susan Banks",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Tom Wolfe",
+            age: 65,
+            gender: "male",
+        },
+        {
+            name: "Jane Banks",
+            age: 60,
+            gender: "female",
+        },
+        {
+            name: "Renee Montoya",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Aaron Ross",
+            age: 40,
+            gender: "male",
+        },
+        {
+            name: "Jack McBrayer",
+            age: 50,
+            gender: "male",
+        },
+        {
+            name: "Laurence Fishburne",
+            age: 60,
+            gender: "male",
+        },
+        {
+            name: "Melinda McGraw",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Betty White",
+            age: 60,
+            gender: "female",
+        },
+        {
+            name: "Jimmy McGill",
+            age: 40,
+            gender: "male",
+        },
     ];
 
-    let listOfMike = [
-        { name: "Walter", age: 45 },
-        { name: "Skyler", age: 40 },
-        { name: "Jesse", age: 37 },
-        { name: "Hank", age: 43 },
-        { name: "Marie", age: 39 },
-        { name: "Saul", age: 46 },
-        { name: "Mike1", age: 55 },
-        { name: "Mike2", age: 55 },
-        { name: "Mike3", age: 55 },
-        { name: "Mike4", age: 55 },
-        { name: "Nacho", age: 34 },
-        { name: "Lydia", age: 30 },
-        { name: "Todd", age: 31 },
-        { name: "Kim", age: 33 },
-        { name: "Gustavo", age: 29 },
-        { name: "Howard", age: 36 },
+    let listOfCarrillo = [
+        {
+            name: "Alberto Véga",
+            age: 40,
+            gender: "male",
+        },
+        {
+            name: "Bob Odenkirk",
+            age: 50,
+            gender: "male",
+        },
+        {
+            name: "Govard Muldoon",
+            age: 60,
+            gender: "male",
+        },
+        {
+            name: "Jesse Pinkman",
+            age: 30,
+            gender: "male",
+        },
+        {
+            name: "Hank Moody",
+            age: 35,
+            gender: "male",
+        },
+        {
+            name: "Marisol Tomei",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Raymond Chandler",
+            age: 65,
+            gender: "male",
+        },
+        {
+            name: "Walt Warren",
+            age: 60,
+            gender: "male",
+        },
+        {
+            name: "José Guadalupe Posada Carrillo1",
+            age: 70,
+            gender: "male",
+        },
+        {
+            name: "José Guadalupe Posada Carrillo2",
+            age: 70,
+            gender: "male",
+        },
+        {
+            name: "José Guadalupe Posada Carrillo3",
+            age: 70,
+            gender: "male",
+        },
+        {
+            name: "José Guadalupe Posada Carrillo4",
+            age: 70,
+            gender: "male",
+        },
+        {
+            name: "Jack Bauer",
+            age: 30,
+            gender: "male",
+        },
+        {
+            name: "Aaron Paul",
+            age: 40,
+            gender: "male",
+        },
+        {
+            name: "Mia Farrow",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Lucas Hedges",
+            age: 60,
+            gender: "male",
+        },
+        {
+            name: "Joanne Whalen",
+            age: 65,
+            gender: "female",
+        },
+        {
+            name: "Walter White",
+            age: 55,
+            gender: "male",
+        },
+        {
+            name: "Oscar-winning actor Walter White",
+            age: 55,
+            gender: "male",
+        },
+        {
+            name: "Susan Banks",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Tom Wolfe",
+            age: 65,
+            gender: "male",
+        },
+        {
+            name: "Jane Banks",
+            age: 60,
+            gender: "female",
+        },
+        {
+            name: "Renee Montoya",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Aaron Ross",
+            age: 40,
+            gender: "male",
+        },
+        {
+            name: "Jack McBrayer",
+            age: 50,
+            gender: "male",
+        },
+        {
+            name: "Laurence Fishburne",
+            age: 60,
+            gender: "male",
+        },
+        {
+            name: "Melinda McGraw",
+            age: 45,
+            gender: "female",
+        },
+        {
+            name: "Betty White",
+            age: 60,
+            gender: "female",
+        },
+        {
+            name: "Jimmy McGill",
+            age: 40,
+            gender: "male",
+        },
     ];
 
     it("should return name of oldest person and middle age of all persons in list", () => {
-        expect(getAgeInfoFromList(personsList)).toBe("Oldest person(s): Tuco / Middle age: 38.3");
+        expect(getAgeInfoFromList(personsList)).toBe(
+            "Oldest person(s): José Guadalupe Posada Carrillo / Middle age: 50.6"
+        );
     }),
         it("should return list with all Mikes and middle age of all persons in list", () => {
-            expect(getAgeInfoFromList(listOfMike)).toBe(
-                "Oldest person(s): Mike1, Mike2, Mike3, Mike4 / Middle age: 41.4"
+            expect(getAgeInfoFromList(listOfCarrillo)).toBe(
+                "Oldest person(s): José Guadalupe Posada Carrillo1, José Guadalupe Posada Carrillo2, José Guadalupe Posada Carrillo3, José Guadalupe Posada Carrillo4 / Middle age: 52.6"
             );
         });
 });
